ca0d8e959588c9d682cb1b2d6a1fd0082d39106c
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-03 21:31:14 +0530

    Add repository CommentRepository.java interface

---
+++ src/main/java/tech/sushnag22/blogrestapi/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package tech.sushnag22.blogrestapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<CommentRepository, Integer> {
+
+}
