00062d61e7d7be58b077bd285224aa26dc8d8cc7
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-03 21:51:18 +0530

    Update repository CommentRepository.java interface to implement pagination

--- src/main/java/tech/sushnag22/blogrestapi/repository/CommentRepository.java
+++ src/main/java/tech/sushnag22/blogrestapi/repository/CommentRepository.java
@@ -1,7 +1,10 @@
 package tech.sushnag22.blogrestapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import tech.sushnag22.blogrestapi.model.Comment;
 
-public interface CommentRepository extends JpaRepository<CommentRepository, Integer> {
-
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    Page<Comment> findByPostId(int postId, Pageable pageable);
 }
